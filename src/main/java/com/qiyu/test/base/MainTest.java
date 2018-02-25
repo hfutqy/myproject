af9959595734708b4a2e360f797084727f087f1e
祁玉 <924120316@qq.com>
2018-02-25 22:27:42 +0800

    Initial commit

---
+++ src/main/java/com/qiyu/test/base/MainTest.java
@@ -0,0 +1,9 @@
+package com.qiyu.test.base;
+
+public class MainTest {
+    public static void main(String[] agrs){
+        System.out.print("xxxx");
+
+
+    }
+}
---
+++ src/main/java/com/qiyu/test/dao/UserInfo.java
@@ -0,0 +1,10 @@
+package com.qiyu.test.dao;
+
+/**
+ * dao数据库访问对象
+ * 封装数据库访问
+ */
+public interface UserInfo {
+
+
+}
