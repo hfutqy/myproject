2fd33ba70e9cf4c70abe038ed84cf5753034f11c
祁玉 <924120316@qq.com>
2018-02-26 10:20:13 +0800

    格式化项目文件
    123

---
+++ src/main/java/com/qiyu/test/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.qiyu.test.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * BaseController
+ * Created by qiyu on 2018/2/26.
+ */
+public class BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+}
