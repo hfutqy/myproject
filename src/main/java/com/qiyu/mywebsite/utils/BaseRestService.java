ef122b411bbafa5c5321061d5af3597fa0aad6bb
祁玉 <924120316@qq.com>
2018-03-12 23:37:43 +0800

    wxss
    有点麻烦

--- src/main/java/com/qiyu/mywebsite/utils/BaseRestService.java
+++ src/main/java/com/qiyu/mywebsite/utils/BaseRestService.java
@@ -1,18 +1,26 @@
 package com.qiyu.mywebsite.utils;
 
-import com.alibaba.fastjson.util.Base64;
+import com.qiyu.mywebsite.constant.BaseConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.Base64Utils;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.URLDecoder;
 
 /**
  * rest
  * Created by qiyu on 2018/2/26.
  */
 public class BaseRestService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseRestService.class);
+
+
     private HttpServletRequest request;
     private HttpServletResponse response;
 
@@ -27,14 +35,23 @@ public class BaseRestService {
         } else {
             String method = this.request.getMethod();
             String ret;
-//            if (!method.equalsIgnoreCase("GET") && !method.equalsIgnoreCase("DELETE")) {
+            if (!method.equalsIgnoreCase("GET") && !method.equalsIgnoreCase("DELETE")) {
                 ret = this.getBodyData();
-//            } else {
-//                ret = this.request.getQueryString();
-//            }
-            return null == ret ? null : new String(Base64Utils.decodeFromString(ret));
+            } else {
+                ret = this.request.getQueryString();
+            }
+            if(!StringUtils.isEmpty(ret)){
+                String result = ret.replace("data=","");
+                //处理URLEncoder数据
+                result = URLDecoder.decode(result, BaseConstants.UTF_8);
+                return new String(Base64Utils.decodeFromString(result));
+            }else {
+                return null;
+            }
+
         }
     }
+
 
     private String getBodyData() throws IOException {
         StringBuilder data = new StringBuilder();
@@ -45,7 +62,7 @@ public class BaseRestService {
                 data.append(line);
             }
         } catch (IOException var8) {
-            throw new IOException(var8.getMessage(), var8.getCause());
+            LOGGER.error(var8.getMessage(), var8.getCause());
         }
         return data.toString();
     }
