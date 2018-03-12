55f39502175dc507ad0041168eea24f2561b0e3a
祁玉 <924120316@qq.com>
2018-03-12 13:47:17 +0800

    入参要URLdecoder解码处理

--- src/main/java/com/qiyu/mywebsite/utils/MainTest.java
+++ src/main/java/com/qiyu/mywebsite/utils/MainTest.java
@@ -1,47 +1,51 @@
 package com.qiyu.mywebsite.utils;
 
+import com.qiyu.mywebsite.constant.BaseConstants;
+
+import java.net.URLDecoder;
+
 /**
  * 测试类
  */
 public class MainTest {
-//    public static void main(String[] agrs) {
-//        int[] arr = {2, 5, 1, 7, 3, 9, 6, 12, 3, 6, 1};
-//        quickSort(arr, 0, arr.length - 1);
-//        for (int e : arr)
-//            System.out.print(e + " ");
-//    }
+    public static void main(String[] agrs) throws Exception {
+        String url = "data=xxx%3d";
+        String ret = url.replace("data=", "");
+        String xx = URLDecoder.decode(ret, BaseConstants.UTF_8);
+        System.out.print(xx);
+    }
 
     /**
      * 加密结果
      */
-    public static void main(String[] agrs) {
-        String str = "{sfadf{{afad{}";
-        char[] strC = str.toCharArray();
-        String endS = "";
-        if(strC.length > 1){
-            //头
-            if(strC[0] == '{' && strC[1] != '{'){
-                endS+='{';
-            }
-            //尾
-            if(strC[strC.length-1] == '{' && strC[strC.length-2] != '{'){
-                endS+='{';
-            }
-        }else if(strC.length==1 && strC[0]=='{'){
-            endS+='{';
-        }
-        //中间
-        for(int i=1; i<strC.length-1; i++){
-            if(strC[i] == '{' && strC[i+1] == '{'){
-                continue;
-            }else if(strC[i] == '{' && strC[i-1] == '{'){
-                continue;
-            }else if(strC[i] == '{'){
-                endS+='{';
-            }
-        }
-        System.out.print(endS);
-    }
+//    public static void main(String[] agrs) {
+//        String str = "{sfadf{{afad{}";
+//        char[] strC = str.toCharArray();
+//        String endS = "";
+//        if(strC.length > 1){
+//            //头
+//            if(strC[0] == '{' && strC[1] != '{'){
+//                endS+='{';
+//            }
+//            //尾
+//            if(strC[strC.length-1] == '{' && strC[strC.length-2] != '{'){
+//                endS+='{';
+//            }
+//        }else if(strC.length==1 && strC[0]=='{'){
+//            endS+='{';
+//        }
+//        //中间
+//        for(int i=1; i<strC.length-1; i++){
+//            if(strC[i] == '{' && strC[i+1] == '{'){
+//                continue;
+//            }else if(strC[i] == '{' && strC[i-1] == '{'){
+//                continue;
+//            }else if(strC[i] == '{'){
+//                endS+='{';
+//            }
+//        }
+//        System.out.print(endS);
+//    }
     /**
      * 快排
      */
