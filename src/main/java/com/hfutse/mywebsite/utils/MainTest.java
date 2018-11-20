a41a36eb3f02d7515ccbca86be3c23b90fc4d129
hfutqy <924120316@qq.com>
2018-11-20 18:33:18 +0800

    2018/11/20 update

--- src/main/java/com/hfutse/mywebsite/utils/MainTest.java
+++ src/main/java/com/hfutse/mywebsite/utils/MainTest.java
@@ -1,28 +1,30 @@
 package com.hfutse.mywebsite.utils;
 
+import java.util.HashMap;
+
 /**
  * 测试类
  */
-public class MainTest {
-    public static void main(String[] agrs) throws Exception {
-        String url = "data=xxx%3dd";
-        if (url.length() > 10) {
-            url = url.substring(0, 10);
-        }
-        System.out.print(url);
-    }
-
-    public int query(int a) {
-        return a;
-    }
-
-    public String query(int b,int c) {
-        return b+"";
-    }
-
-    /**
-     * 加密结果
-     */
+//public class MainTest {
+//    public static void main(String[] agrs) throws Exception {
+//        String url = "data=xxx%3dd";
+//        if (url.length() > 10) {
+//            url = url.substring(0, 10);
+//        }
+//        System.out.print(url);
+//    }
+//
+//    public int query(int a) {
+//        return a;
+//    }
+//
+//    public String query(int b,int c) {
+//        return b+"";
+//    }
+//
+//    /**
+//     * 加密结果
+//     */
 //    public static void main(String[] agrs) {
 //        String str = "{sfadf{{afad{}";
 //        char[] strC = str.toCharArray();
@@ -51,34 +53,69 @@ public class MainTest {
 //        }
 //        System.out.print(endS);
 //    }
-    /**
-     * 快排
-     */
-    static void quickSort(int[] arr, int low, int high) {
-        int l = low;
-        int h = high;
-        int key = arr[l];
-        while (l < h) {
-            while (l < h && arr[h] > key)//处理右侧比Key大
-                h--;
-            if (l < h) {
-                int tmp = arr[h];
-                arr[h] = arr[l];
-                arr[l] = tmp;
-                l++;
-            }
-            while (l < h && arr[l] < key)//处理左侧比key小
-                l++;
-            if (l < h) {
-                int tmp = arr[h];
-                arr[h] = arr[l];
-                arr[l] = tmp;
-                h--;
-            }
+//
+public class MainTest {
+    public static void main(String[] args) {
+        int n=10;
+        System.out.println(getWays(n));
+    }
+
+    static int getWays(int n) {
+        int f1 = 1;
+        int f2 = 2;
+        int count = 0;
+        for(int i = 3; i<=n; i++){
+            count = f1+f2;
+            f1 = f2;
+            f2 = count;
         }
-        if (l > low)
-            quickSort(arr, low, l - 1);
-        if (h < high)
-            quickSort(arr, h + 1, high);
+        return count;
     }
 }
+//    public static void main(String[] args) {
+//        int[] a = {2, 3, 5, 6, 9, 10, 13};
+//        int[] b = {7, 8, 9, 10, 11, 12};
+//        System.out.println(findMedianSortedArrays(a,b));
+//    }
+//    public static double findMedianSortedArrays(int[] A, int[] B) {
+//        int m = A.length;
+//        int n = B.length;
+//        if (m > n) { // to ensure m<=n
+//            int[] temp = A; A = B; B = temp;
+//            int tmp = m; m = n; n = tmp;
+//        }
+//        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+//        int count=0;
+//        while (iMin <= iMax) {
+//            count++;
+//            int i = (iMin + iMax) / 2;
+//            int j = halfLen - i;
+//            if (i < iMax && B[j-1] > A[i]){
+//                iMin = i + 1; // i is too small
+//            }
+//            else if (i > iMin && A[i-1] > B[j]) {
+//                iMax = i - 1; // i is too big
+//            }
+//            else { // i is perfect
+//                int maxLeft = 0;
+//                if (i == 0) { maxLeft = B[j-1]; }
+//                else if (j == 0) { maxLeft = A[i-1]; }
+//                else { maxLeft = Math.max(A[i-1], B[j-1]); }
+//                if ( (m + n) % 2 == 1 ) {
+//                    System.out.println(count+"is count");
+//                    return maxLeft;
+//                }
+//
+//                int minRight = 0;
+//                if (i == m) { minRight = B[j]; }
+//                else if (j == n) { minRight = A[i]; }
+//                else { minRight = Math.min(B[j], A[i]); }
+//
+//                System.out.println(count+"is count");
+//                return (maxLeft + minRight) / 2.0;
+//            }
+//        }
+//        System.out.println(count+"is count");
+//        return 0.0;
+//    }
+//}
